5cc58cc962b6ff4f96994e94c5ea65f4e15e66be
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2022-02-15 17:53:29 +0100

    Firabase integration

---
+++ Assets/Scripts/FirebaseData.cs
@@ -0,0 +1,146 @@
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Extensions;
+using UnityEngine.UI;
+
+public class FirebaseData : MonoBehaviour
+{
+    FirebaseAuth auth;
+    public Text infoText;
+
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.Exception != null)
+                Debug.LogError(task.Exception);
+
+            auth = FirebaseAuth.DefaultInstance;
+            // RegisterNewUser("test1@test.test", "Test123!");
+            // RegisterNewUser("test2@test.test", "Test123!");
+            // RegisterNewUser("test3@test.test", "Test123!");
+            // RegisterNewUser("test4@test.test", "Test123!");
+        });
+    }
+
+    public void SignInTestUser(string email = "test1@test.test")
+    {
+        Debug.Log("clicked");
+        SignIn(email, "Test123!");
+    }
+    
+    
+    private void RegisterNewUser(string email, string password)
+    {
+        Debug.Log("Starting Registration");
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.Exception != null)
+            {
+                Debug.LogWarning(task.Exception);
+            }
+            else
+            {
+                FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User Registerd: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+            }
+        });
+    }
+
+    private void SignIn(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.Exception != null)
+            {
+                Debug.LogWarning(task.Exception);
+            }
+            else
+            {
+                FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+                infoText.text = newUser.Email;
+                LoadFromFirebase();
+            }
+        });
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            AnonymousSignIn();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SignOut();
+        
+        if (Input.GetKeyDown(KeyCode.D))
+            DataTest(auth.CurrentUser.UserId, Random.Range(0, 100).ToString());
+    }
+
+    private void SignOut()
+    {
+        auth.SignOut();
+        Debug.Log("User signed out");
+    }
+    private void AnonymousSignIn()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWithOnMainThread(task => {
+            if (task.Exception != null)
+            {
+                Debug.LogWarning(task.Exception);
+            }
+            else
+            {
+                FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+            }
+        });
+    }
+
+    private void DataTest(string userID, string data)
+    {
+        Debug.Log("Trying to write data...");
+        var db = FirebaseDatabase.DefaultInstance;
+        db.RootReference.Child("users").Child(userID).SetValueAsync(data).ContinueWithOnMainThread(task =>
+        {
+            if (task.Exception != null)
+                Debug.LogWarning(task.Exception);
+            else
+                Debug.Log("DataTestWrite: Complete");
+        });
+    }
+    
+    private void SaveToFirebase(string data)
+    {
+        var db = FirebaseDatabase.DefaultInstance;
+        var userId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
+        //puts the json data in the "users/userId" part of the database.
+        db.RootReference.Child("users").Child(userId).SetRawJsonValueAsync(data);
+    }
+    
+    private void LoadFromFirebase()
+    {
+        var db = FirebaseDatabase.DefaultInstance;
+        var userId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
+        db.RootReference.Child("users").Child(userId).GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.Exception != null)
+            {
+                Debug.LogError(task.Exception);
+            }
+
+            //here we get the result from our database.
+            DataSnapshot snap = task.Result;
+
+            infoText.text = snap.GetRawJsonValue();
+        });
+    }
+    
+}
